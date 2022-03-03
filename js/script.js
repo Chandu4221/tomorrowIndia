4cc71b945ef04f255ccc3dd2ddba924983b433ab
dchandra <chndraja@gmail.com>
2022-03-03 11:02:15 +0530

    homepage slider completed

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".homeSlider", {
+  speed: 400,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: ".homeSlider-next",
+    prevEl: ".homeSlider-prev",
+  },
+  pagination: {
+    el: ".homeSlider-pagination",
+    clickable: true,
+  },
+});

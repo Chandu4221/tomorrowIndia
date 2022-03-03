af7bb6b90e7b188559481818c97850155cfd64b0
dchandra <chndraja@gmail.com>
2022-03-03 13:44:11 +0530

    completed trending now slider

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".homeSlider", {
+const homeSlider = new Swiper(".homeSlider", {
   speed: 400,
   spaceBetween: 0,
   navigation: {
@@ -14,4 +14,30 @@ const swiper = new Swiper(".homeSlider", {
 var elem = document.querySelector(".grid");
 var msnry = new Masonry(elem, {
   itemSelector: ".grid-item",
+});
+
+const trensingNowSlider = new Swiper(".trendingNow-slider", {
+  speed: 400,
+  slidesPerView: 1,
+  spaceBetween: 15,
+  centeredSlides: true,
+  loop: true,
+  navigation: {
+    nextEl: ".trendingNow-slider-next",
+    prevEl: ".trendingNow-slider-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
 });
